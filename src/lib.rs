2ec7ee4664e930e7024d0e7aa9d962619085e1c3
Yubin Yang <eric@Yubins-MacBook-Air.local>
2022-11-22 01:01:35 +0800

    add initial commit

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#![doc(html_logo_url = "https://d30y9cdsu7xlg0.cloudfront.net/png/411962-200.png")]
+/// bit as output
+pub fn and(a: u8, b: u8) -> u8 {
+    match (a, b) {
+        (1, 1) => 1,
+        _ => 0
+    }
+}
+
+/// a bit as output
+pub fn xor(a: u8, b: u8) -> u8 {
+    match (a, b) {
+        (1, 1) | (0, 0) => 0,
+        _ => 1,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{xor, and};
+    #[test]
+    fn test_and() {
+        assert_eq!(1, and(1, 1));
+        assert_eq!(0, and(0, 1));
+        assert_eq!(0, and(1, 0));
+        assert_eq!(0, and(0, 0));
+    }
+
+    #[test]
+    fn test_xor() {
+        assert_eq!(1, xor(1, 0));
+        assert_eq!(0, xor(0, 0));
+        assert_eq!(0, xor(1, 1));
+        assert_eq!(1, xor(0, 1));
+    }
+}
